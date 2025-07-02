568d2988f6962f530845a9da7b127843e8ad7e45
Ninglo <jiujianian@gmail.com>
2025-07-02 14:08:35 +0800

    feat: export module

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,4 @@
+import * as UI from '../../../src/components/ui';
+export default UI;
+
+export * from '../../../src/components/ui'; 
---
+++ packages/ui/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    dts: true,
+    splitting: false,
+    clean: true,
+    sourcemap: true,
+    external: [
+        'react',
+        'react-dom',
+    ],
+}); 
